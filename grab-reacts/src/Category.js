dc789765d303cbbd703e153b5ec73e42f4931606
daeneerys <johndaniel.ocampo.cics@ust.edu.ph>
2024-02-19 15:28:50 +0800

    -

--- grab-reacts/src/Category.js
+++ grab-reacts/src/Category.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Category = (props) => {
+  const [isHovered, setIsHovered] = useState("");
   return (
-           <div className='category'>
-         <img src = {props.categoryObj.photoName} alt = {props.name}></img>
-         <h3>{props.categoryObj.name}</h3>
-     </div>
-  )
-}
+    <div className="category">
+      <img
+        src={
+          isHovered
+            ? props.categoryObj.secondPhoto
+            : props.categoryObj.photoName
+        }
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        alt={props.name}
+      ></img>
+      {isHovered ? (
+        <h3 className="green">{props.categoryObj.name}</h3>
+      ) : (
+        <h3
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {props.categoryObj.name}{" "}
+        </h3>
+      )}
+    </div>
+  );
+};
 
-
-export default Category
+export default Category;
--- grab-reacts/src/CategoryMenu.js
+++ grab-reacts/src/CategoryMenu.js
@@ -17,82 +17,102 @@ export default CategoryMenu;
 const categoryData = [
     {
         name: "Japanese",
-        photoName: "assets/Japanese.jpg"
+        photoName: "assets/Japanese.jpg",
+        secondPhoto: "assets/Hjapanese.jpg"
     },
     {
         name: "Beef",
-        photoName: "assets/Beef.jpg"
+        photoName: "assets/Beef.jpg",
+        secondPhoto: "assets/Hbeef.jpg"
     },
     {
         name: "Burgers",
-        photoName: "assets/Burgers.jpg"
+        photoName: "assets/Burgers.jpg",
+        secondPhoto: "assets/Hburger.jpg"
     },
     {
         name: "Filipino",
-        photoName: "assets/Filipino.jpg"
+        photoName: "assets/Filipino.jpg",
+        secondPhoto: "assets/Hfilipino.jpg"
     },
     {
         name: "Italian",
-        photoName: "assets/Italian.jpg"
+        photoName: "assets/Italian.jpg",
+        secondPhoto: "assets/Hitalian.jpeg"
     },
     {
         name: "Pizza",
-        photoName: "assets/Pizza.jpg"
+        photoName: "assets/Pizza.jpg",
+        secondPhoto: "assets/Hpizza.jpg"
     },
     {
         name: "Quick Bites",
-        photoName: "assets/Quick Bites.jpg"
+        photoName: "assets/Quick Bites.jpg",
+        secondPhoto: "assets/Hquickbites.jpg"
     },
     {
         name: "Chinese",
-        photoName: "assets/Chinese.jpg"
+        photoName: "assets/Chinese.jpg",
+        secondPhoto: "assets/Hchinese.jpg"
     },
     {
         name: "Breakfast & Brunch",
-        photoName: "assets/Breakfast.jpg"
+        photoName: "assets/Breakfast.jpg",
+        secondPhoto: "assets/Hbreakfast.jpg"
     },
     {
         name: "Snack",
-        photoName: "assets/Snack.jpg"
+        photoName: "assets/Snack.jpg",
+        secondPhoto: "assets/Hsnack.jpg"
     },
     {
         name: "American",
-        photoName: "assets/American.jpg"
+        photoName: "assets/American.jpg",
+        secondPhoto: "assets/Hamerican.jpg"
     },
     {
         name: "Casual Dining",
-        photoName: "assets/Casual.jpg"
+        photoName: "assets/Casual.jpg",
+        secondPhoto: "assets/Hcasual.jpg"
     },
     {
         name: "Kiosk",
-        photoName: "assets/Kiosk.jpg"
+        photoName: "assets/Kiosk.jpg",
+        secondPhoto: "assets/Hkiosk.jpeg"
     },
     {
         name: "Fast Food",
-        photoName: "assets/Fast.jpg"
+        photoName: "assets/Fast.jpg",
+        secondPhoto: "assets/Hfast.jpg"
     },
     {
         name: "Milk Tea",
-        photoName: "assets/MilkTea.jpg"
+        photoName: "assets/MilkTea.jpg",
+        secondPhoto: "assets/Hmilktea.png"
     },
     {
         name: "Asian",
-        photoName: "assets/Asian.jpg"
+        photoName: "assets/Asian.jpg",
+        secondPhoto: "assets/Hasian.jpg"
     },
     {
         name: "Bakery",
-        photoName: "assets/Bakery.jpg"
+        photoName: "assets/Bakery.jpg",
+        secondPhoto: "assets/Hbakery.jpg"
     },
     {
         name: "Pork",
-        photoName: "assets/Pork.jpg"
+        photoName: "assets/Pork.jpg",
+        secondPhoto: "assets/Hpork.jpg"
     },
     {
         name: "Beverages",
-        photoName: "assets/Beverages.jpg"
+        photoName: "assets/Beverages.jpg",
+        secondPhoto: "assets/Hbeverages.jpg"
     },
     {
         name: "Chicken",
-        photoName: "assets/Chicken.jpg"
+        photoName: "assets/Chicken.jpg",
+        secondPhoto: "assets/Hchicken.jpg"
     },
 ]
